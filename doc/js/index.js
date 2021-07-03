5c22032377516c18a6a9576fe3adc5f13b23a0f9
godwin nilla <godwinnillah@gmail.com>
2021-07-03 08:19:11 -0700

    first commit

---
+++ doc/js/index.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if(this.scrollY > 20){
+            $('.navbar').addClass("sticky");
+        }else{
+            $('.navbar').removeClass("sticky");
+        }
+    })
+    $('.menu-2').click(function(){
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-2 i').toggleClass("active");
+    });
+});
