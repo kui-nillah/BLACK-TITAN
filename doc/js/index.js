6b2fcbf48a4ad965862deaf1fbaf4aa0528142b9
godwin nilla <godwinnillah@gmail.com>
2021-07-04 11:20:03 -0700

    second commit

--- doc/js/index.js
+++ doc/js/index.js
@@ -1,12 +1,12 @@
-$(document).ready(function(){
-    $(window).scroll(function(){
-        if(this.scrollY > 20){
+$(document).ready(function() {
+    $(window).scroll(function() {
+        if (this.scrollY > 20) {
             $('.navbar').addClass("sticky");
-        }else{
+        } else {
             $('.navbar').removeClass("sticky");
         }
-    })
-    $('.menu-2').click(function(){
+    });
+    $('.menu-2').click(function() {
         $('.navbar .menu').toggleClass("active");
         $('.menu-2 i').toggleClass("active");
     });
